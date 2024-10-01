c5d0975120779c40568b873833ceb545bee538e6
Eleaha <ellieharrington8@gmail.com>
2024-10-01 19:20:10 +0100

    fix issued with tab size change on reload and slight refactors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,32 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export function Footer() {
-const [activeTab, setActiveTab] = useState("About")
+	const [activeTab, setActiveTab] = useState("About");
 
-function handleTabChange(e) {
-	console.log(e.target.innerText, e.target)
-	setActiveTab(e.target.innerText)
-}
+	function handleTabChange(e: any) {
+		setActiveTab(e.target.innerText);
+	}
+
+	useEffect(() => {
+		const currentPath = window.location.pathname;
+		console.log(currentPath);
+		if (currentPath === "/") {
+			setActiveTab("About");
+		} else if (currentPath === "/cv") {
+			setActiveTab("CV");
+		} else {
+			const currentTab =
+				currentPath[1].toUpperCase() + currentPath.slice(2, currentPath.length);
+			setActiveTab(currentTab);
+		}
+	}, []);
 
 	return (
 		<footer className="footer">
-			<Link to="/" className={`footer-tab ${activeTab === "About" ? "active" : "inactive"}`} id="about-tab" onClick={handleTabChange}>
+			<Link
+				to="/"
+				className={`footer-tab ${activeTab === "About" ? "active" : "inactive"}`}
+				id="about-tab"
+				onClick={handleTabChange}
+			>
 				<div>
 					<p className="tab-text">About</p>
 				</div>
 			</Link>
-			<Link to="/projects" className={`footer-tab ${activeTab === "Projects" ? "active" : "inactive"}`} id="projects-tab" onClick={handleTabChange}>
+			<Link
+				to="/projects"
+				className={`footer-tab ${activeTab === "Projects" ? "active" : "inactive"}`}
+				id="projects-tab"
+				onClick={handleTabChange}
+			>
 				<div>
 					<p className="tab-text">Projects</p>
 				</div>
 			</Link>
-			<Link to="/cv" className={`footer-tab ${activeTab === "CV" ? "active" : "inactive"}`} id="cv-tab" onClick={handleTabChange}>
+			<Link
+				to="/cv"
+				className={`footer-tab ${activeTab === "CV" ? "active" : "inactive"}`}
+				id="cv-tab"
+				onClick={handleTabChange}
+			>
 				<div>
 					<p className="tab-text">CV</p>
 				</div>
 			</Link>
-			<Link to="/contact" className={`footer-tab ${activeTab === "Contact" ? "active" : "inactive"}`} id="contact-tab" onClick={handleTabChange}>
+			<Link
+				to="/contact"
+				className={`footer-tab ${activeTab === "Contact" ? "active" : "inactive"}`}
+				id="contact-tab"
+				onClick={handleTabChange}
+			>
 				<div>
 					<p className="tab-text">Contact</p>
 				</div>
@@ -34,7 +67,3 @@ function handleTabChange(e) {
 		</footer>
 	);
 }
-
-//create state to keep stack of the active tab
-//add active class to active tabs
-//change width to something like 15 vs 40 active
--- src/components/sub-components/Project-card.tsx
+++ src/components/sub-components/Project-card.tsx
@@ -2,10 +2,6 @@ import { ProjectCardProps } from "../../interfaces";
 import { ClickThroughSlideshow } from "./Click-through-slideshow";
 import { TechTag } from "./Tech-tag";
 
-//iterate through the project-index
-//for each project
-// slideshow on hover
-
 export function ProjectCard({ project }: ProjectCardProps) {
 	return (
 		<figure className="project-card">
