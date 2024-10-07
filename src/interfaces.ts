5b5d64ea3eb43fbf0b781bd415cfc3168b342118
Eleaha <ellieharrington8@gmail.com>
2024-10-07 13:41:54 +0100

    add basic structure to CV page

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,17 +1,36 @@
 export interface Project {
-    name: string;
-    techStack: string[];
-    imgAddresses: string[];
-    dates: string;
-    description: string
+	name: string;
+	techStack: string[];
+	imgAddresses: string[];
+	dates: string;
+	description: string;
 }
 
 export interface slideShowProps {
-    images: string[];
-    delay: number;
-    // text?: string[];
+	images: string[];
+	delay: number;
+	// text?: string[];
 }
 
 export interface ProjectCardProps {
-    project: Project
+	project: Project;
+}
+
+export interface SkillCategory {
+	category: string;
+	skills: string[];
+}
+
+export interface Role {
+	title: string;
+	where: string;
+	when: string;
+	description: string[];
+}
+
+export interface Education {
+    where: string;
+    completed: number;
+    level: string;
+    subjects?: string[]
 }
